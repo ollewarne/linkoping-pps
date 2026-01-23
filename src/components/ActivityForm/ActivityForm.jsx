8120e89cffda06e1505f516f1d1cbf5962db4c4c
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-01-23 14:51:08 +0100

    create a base layout and placeholder components to start from

---
+++ src/components/ActivityForm/ActivityForm.jsx
@@ -0,0 +1,9 @@
+import styles from "./ActivityForm.module.css"
+
+function ActivityForm() {
+    return (
+        <div className={styles.container}><h2>Activity Form</h2></div>
+    )
+}
+
+export default ActivityForm;
