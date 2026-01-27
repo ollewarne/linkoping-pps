d0ec83ecc6be1684b8b0f80b2dca2b59f8974e4f
soew <209520777+ollewarne@users.noreply.github.com>
2026-01-27 11:33:14 +0100

    add activities state to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import './App.css'
 import ActivityForm from './components/ActivityForm/ActivityForm'
 import Header from './components/Header/Header'
@@ -6,16 +7,17 @@ import WorkDayForm from './components/WorkDayForm/WorkDayForm'
 
 
 function App() {
+    const [activities, setActivities] = useState([]);
 
     return (
         <>
             <Header />
             <div style={{display: "flex", flexDirection: "column", gap: "1em"}}>
                 <WorkDayForm />
-                <ActivityForm />
+                <ActivityForm activities={activities} setActivities={setActivities} />
             </div>
             <div>
-                <Schedule />
+                <Schedule activities={activities} setActivities={setActivities} />
             </div>
         </>
     )
