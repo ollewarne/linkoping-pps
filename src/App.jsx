e25c7f85fc52689fdc11a1ffd8fdb05a15e8f293
mattssonmart <mattsson112.mm@gmail.com>
2026-02-02 15:36:08 +0100

    pause module + button in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import './App.css'
-import ActivityForm from './components/ActivityForm/ActivityForm'
-import Header from './components/Header/Header'
-import Schedule from './components/Schedule/Schedule'
-import WorkDayForm from './components/WorkDayForm/WorkDayForm'
-
+import './App.css';
+import { useState } from 'react';
+import ActivityForm from './components/ActivityForm/ActivityForm';
+import Header from './components/Header/Header';
+import Schedule from './components/Schedule/Schedule';
+import WorkDayForm from './components/WorkDayForm/WorkDayForm';
+import PauseStatistics from './components/PauseStatistics/PauseStatistics';
 
 function App() {
+  const [showPauseModal, setShowPauseModal] = useState(false);
 
-    return (
-        <>
-            <Header />
-            <div style={{display: "flex", flexDirection: "column", gap: "1em"}}>
-                <WorkDayForm />
-                <ActivityForm />
-            </div>
-            <div>
-                <Schedule />
-            </div>
-        </>
-    )
+  const handlePauseSave = (data) => {
+    console.log("Saved:", data);
+    setShowPauseModal(false); // close modal after save
+  };
+
+  return (
+    <>
+      <Header />
+      <div style={{ display: "flex", flexDirection: "column", gap: "1em" }}>
+        <WorkDayForm />
+        <ActivityForm />
+
+        <button onClick={() => setShowPauseModal(true)}>
+          Open Pause Statistics
+        </button>
+
+        {showPauseModal && (
+          <PauseStatistics onSave={handlePauseSave} onClose={() => setShowPauseModal(false)} />
+        )}
+      </div>
+
+      <div>
+        <Schedule />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
