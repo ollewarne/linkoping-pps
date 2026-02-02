453a1371cc280b5c816335dc5cb35107e8c32ef2
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-02-02 15:34:45 +0100

    adding library for static userOptions

---
+++ src/constants/userOptions.js
@@ -0,0 +1,17 @@
+export const userOptions = {
+
+en: {
+    workEnvironment: ['Home', 'Private office', 'Shared office', 'Open-plan office', 'Public place', 'Hybrid work', 'Other'],
+
+    category: ['Administrative', 'Creative', 'Technical', 'Analytical', 'Communication', 'Planning', 'Learning','Sales & Marketing', 'Support', 'Operations', 'Other'],
+
+    impacts: ['Tired', 'Hungry', 'Unfocused', 'Restless', 'Distracted by others', 'Distracted by noise','Distracted by notifications', 'Procrastination', 'Poor internet connection', 'Energized', 'Focused', 'Stressed', 'Other']
+},
+
+sv: {
+    workEnvironment: ['Hemma', 'Eget kontor', 'Delat kontor', 'Öppet kontorslandskap', 'Publik plats', 'Hybridarbete','Annat'],
+
+    category: ['Administrativt', 'Kreativt', 'Tekniskt', 'Analytiskt', 'Kommunikation', 'Planering', 'Lärande', 'Försäljning & marknadsföring', 'Support', 'Verksamhet / Drift', 'Annat'],
+
+    impacts: ['Trött', 'Hungrig', 'Ofokuserad', 'Rastlös', 'Störd av andra', 'Störd av ljud', 'Störd av notiser','Prokrastinering', 'Dålig uppkoppling', 'Energifylld', 'Fokuserad', 'Stressad', 'Övrigt']
+}}
