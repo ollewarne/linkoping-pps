c9be85775157087fa86c9f4a46d7712c36d15fa0
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-02-02 15:35:44 +0100

    adding library for language

---
+++ src/locales/language.js
@@ -0,0 +1,87 @@
+export const languageLibrary = {
+    en: {
+    // ----- ÅTERKOMMANDE -----
+    start: 'Start',
+    end: 'End',
+    save: 'Save',
+    add: 'Add',
+    hours: 'Hours',
+    minutes: 'Minutes',
+    active: 'Active',
+    break: 'Break',
+
+    // ----- FORM 1 ----- 
+    form1Header: 'Register Workday',
+    // fieldset 1
+    form1WorkH: 'Working hours',
+    form1WorkHExplanation: 'Explanation???',
+    // fieldset 2
+    form1Checkbox: 'I have non-working hours to register (example: lunchbreak)',
+    nonWorkH: 'Non-working hours',
+    nonWorkHExplanation: 'Explanation???',
+    // fieldset 3
+    workEnvironment: 'Work Environment',
+    workEnvironmentDefault: 'Select an environment',
+
+    // ----- FORM 2 ----- 
+    form2Header: 'Register Activity',
+    // fieldset 1
+    form2Activity: 'Activity',
+    form2Category: 'Category',
+    form2CategoryDefault: 'Pick a category',
+    form2Title: 'Title',
+    form2TitleDefatul: 'Enter a title',
+    // fieldset 2
+    form2Rank: 'Rank of importance',
+    // fieldset 3
+    form2EstimatedDuration: 'Estimated Duration',
+    // fieldset 4
+    form2TimeStructure: 'Time Structure',
+    // submit
+    form2Submit: 'Add Activity'
+},
+
+sv: {
+    // ----- ÅTERKOMMANDE -----
+    start: 'Start',
+    end: 'Slut',
+    save: 'Spara',
+    add: 'Lägg till',
+    hours: 'Timmar',
+    minutes: 'Minuter',
+    active: 'Aktiv',
+    break: 'Paus',
+
+    // ----- FORM 1 ----- 
+    form1Header: 'Registera Arbetsdag',
+    // fieldset 1
+    form1WorkH: 'Arbetstid',
+    form1WorkHExplanation: 'Förklaring???',
+    // fieldset 2
+    form1Checkbox: 'Jag har arbetsfri tid att registrera (exempel: lunch)',
+    nonWorkH: 'Arbetsfri Tid',
+    nonWorkHExplanation: 'Förklaring???',
+    // fieldset 3
+    workEnvironment: 'Arbetsmiljö',
+    workEnvironmentDefault: 'Välj en arbetsmiljö',
+
+    // ----- FORM 2 ----- 
+    form2Header: 'Registera Aktivitet',
+    // fieldset 1
+    form2Activity: 'Aktivitet',
+    form2Category: 'Kategori',
+    form2CategoryDefault: 'Välj en kategori',
+    form2Title: 'Titel',
+    form2TitleDefatul: 'Fyll i en titel',
+    // fieldset 2
+    form2Rank: 'Rankning',
+    // fieldset 3
+    form2EstimatedDuration: 'Uppskattad tidsåtgång',
+    // fieldset 4
+    form2TimeStructure: 'Tidsstruktur',
+    // submit
+    form2Submit: 'Lägg till aktivitet'
+}
+
+}
+
