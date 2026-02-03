987a892bdfb07a2d9f00bf1362253844a2dcbe5f
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-02-03 10:43:53 +0100

    inserting toggle language button

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ActivityProvider } from "./contexts/activityContext.jsx"
+import { TranslatePage } from './contexts/languageContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <ActivityProvider>
-            <App />
-        </ActivityProvider>
+        <TranslatePage>
+            <ActivityProvider>
+                <App />
+            </ActivityProvider>
+        </TranslatePage>
     </StrictMode>,
 )
