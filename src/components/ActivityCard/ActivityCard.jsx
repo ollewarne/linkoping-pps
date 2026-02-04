dd49e2e77d6acae9ed4e17e27a8460c565d530a1
soew <209520777+ollewarne@users.noreply.github.com>
2026-02-04 16:37:17 +0100

    add use effect to update the scheduled time for the card on mount

--- src/components/ActivityCard/ActivityCard.jsx
+++ src/components/ActivityCard/ActivityCard.jsx
@@ -1,21 +1,51 @@
 import styles from "./ActivityCard.module.css"
+import { useEffect } from "react";
+import { useActivities } from "../../contexts/activityContext";
 
 const workdayMinutes = 300;
 
 const colors = {
-  "1": "#FFB3BA",
-  "2": "#FFCBA4",
-  "3": "#FFF5BA",
-  "4": "#BAE1FF",
-  "5": "#D3D3D3",
+    "1": "#FFB3BA",
+    "2": "#FFCBA4",
+    "3": "#FFF5BA",
+    "4": "#BAE1FF",
+    "5": "#D3D3D3",
 };
 
-function ActivityCard({activity = {}}) {
+function ActivityCard({ activity = {}, scheduledTime }) {
+
+    const { dispatch } = useActivities();
+
+    // kommer användas senare, ignoreras just nu
+    const handleEdit = () => {
+
+        dispatch({
+            type: "EDIT_ACTIVITY",
+            payload: {
+                id: activity.id,
+                title: newTitle,
+                category: activity.category,
+                ranking: activity.ranking,
+                estimatedDuration: activity.estimatedDuration
+            }
+        });
+    };
+
+    //updaterar tiden den ligger i schemat när den skapas
+    useEffect(() => {
+        dispatch({
+            type: "SCHEDULE_ACTIVITY",
+            payload: {
+                id: activity.id,
+                scheduledTime: scheduledTime
+            }
+        });
+    }, []);
 
     const height = Math.floor((activity.estimatedDuration / workdayMinutes) * 100)
 
     return (
-        <div style={{paddingLeft: "1em", border: "2px solid black", height: `${height}%`, backgroundColor: `${colors[activity.ranking]}`, width: "90%", color: "black", borderRadius: "8px"}}>
+        <div style={{ paddingLeft: "1em", border: "2px solid black", height: `${height}%`, backgroundColor: `${colors[activity.ranking]}`, width: "90%", color: "black", borderRadius: "8px" }}>
             <p>{activity.category} <strong>| </strong>{activity.title}</p>
             <p><strong>Estimated duration: </strong>{activity.estimatedDuration} min</p>
         </div>
