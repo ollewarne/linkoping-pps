ff3c5d26ae41e58723218c772a13fd39cd555965
soew <209520777+ollewarne@users.noreply.github.com>
2026-02-09 15:44:18 +0100

    move dnd button into button container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,15 @@ import DndToggle from "../PopupManager/DndToggle";
 import { ChangeLanguage } from '../ChangeLanguage/ChangeLanguage';
 import ColorMode from '../ThemeToggle/ThemeToggle';
 
-function Header({children,isDndEnabled,toggleDnd}) {
+function Header({ children, isDndEnabled, toggleDnd }) {
     return (
         <header className={styles.container}>
             <h1>BAE Productivity</h1>
-             <DndToggle isOn={isDndEnabled} onToggle={toggleDnd} />
-             <div className={styles.buttonGroup}>
-            <ChangeLanguage />
-            <ColorMode />
-            {children}
+            <div className={styles.buttonGroup}>
+                <DndToggle isOn={isDndEnabled} onToggle={toggleDnd} />
+                <ChangeLanguage />
+                <ColorMode />
+                {children}
             </div>
         </header>
     );
