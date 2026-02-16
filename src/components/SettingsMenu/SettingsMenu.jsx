1dbecb8142ab0180a0911a43ee113778653e1c00
soew <209520777+ollewarne@users.noreply.github.com>
2026-02-16 20:00:15 +0100

    add first part for a settings menu

---
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -0,0 +1,11 @@
+import WorkDayForm from "../WorkDayForm/WorkDayForm";
+import { useState, useEffect } from "react";
+
+function SettingsMenu() {
+
+    return (
+        <WorkDayForm />
+    )
+}
+
+export default SettingsMenu;
