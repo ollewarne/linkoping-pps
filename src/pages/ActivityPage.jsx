8525ac4f593d5a1b915df4f06ce554f6d7e05e1a
soew <209520777+ollewarne@users.noreply.github.com>
2026-02-16 19:53:12 +0100

    update pages to work with routes

---
+++ src/pages/ActivityPage.jsx
@@ -0,0 +1,12 @@
+import ActivityForm from "../components/ActivityForm/ActivityForm";
+import WorkDayForm from "../components/WorkDayForm/WorkDayForm";
+
+function ActivityPage() {
+    return (
+        <>
+            <ActivityForm />
+        </>
+    )
+}
+
+export default ActivityPage;
