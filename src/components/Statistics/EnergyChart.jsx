84a37bea06093a21083d816cb1d493f7318b7545
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-02-19 12:05:07 +0100

    render EnergyChart

---
+++ src/components/Statistics/EnergyChart.jsx
@@ -0,0 +1,85 @@
+import { LineChart } from '@mui/x-charts/LineChart';
+import { useMemo } from 'react';
+import { getAverageStats } from './getAverageStats';
+
+
+
+// function getAverageEnergy(data) {
+//   const timeSlot = {};
+
+//   Object.values(data).forEach((day) => {
+
+//     day.activities.forEach((activity) => {
+
+//       if (!activity.statistics) return;
+
+//       Object.entries(activity.statistics).forEach(([time, statData]) => {
+//         const roundedTime = roundTime(time);
+
+//         if(!timeSlot[roundedTime]) {
+//           timeSlot[roundedTime] = [];
+//         };
+
+//         timeSlot[roundedTime].push(statData.efficiency);
+
+//       });
+
+//     });
+
+//   });
+
+
+// const averages = Object.entries(timeSlot).map(([time, values]) => {
+//   const averageValue = values.reduce((sum, val) => sum + val, 0) / values.length;
+
+//   return {
+//     time,
+//     average: Number(averageValue.toFixed(2))
+//   };
+// });
+
+// averages.sort((a,b) => a.time.localeCompare(b.time));
+
+// return averages;
+
+// };
+
+
+export default function EnergyChart({mockData}) {
+
+  const data = useMemo(
+    () => getAverageStats(mockData, 'efficiency'), [mockData]
+  );
+
+  const xLables = data.map((d) => d.time);
+  const yValues = data.map((d) => d.average);
+
+  return (
+    <LineChart
+      xAxis={[
+        {
+          scaleType: 'point',
+          data: xLables,
+          label: 'Time'
+        }
+      ]}
+      yAxis={[
+        {
+          min: 1,
+          max: 5,
+          tickNumber: 5,
+          label: 'Score'
+        }
+      ]}
+      series = {[
+        {
+          data: yValues,
+          label: 'Average Energy',
+          color: '#f50057'
+        }
+      ]}
+      height={350}
+    />
+  );
+
+}
