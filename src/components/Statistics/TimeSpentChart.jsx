f656f1f90205b993a63fc4d7177647491dd3041a
soew <209520777+ollewarne@users.noreply.github.com>
2026-02-20 14:43:11 +0100

    add donut chart to show time spent on each category

---
+++ src/components/Statistics/TimeSpentChart.jsx
@@ -0,0 +1,42 @@
+import { mockData } from "../../constants/mockData";
+import { PieChart } from "@mui/x-charts";
+
+const aggregatedData = Object.values(mockData).reduce((acc, day) => {
+    day.activities.forEach(activity => {
+        const { category, totalTimeSpent } = activity;
+        acc[category] = (acc[category] || 0) + totalTimeSpent;
+    });
+    return acc;
+}, {});
+
+const formatTime = (minutes) => {
+    const h = Math.floor(minutes / 60);
+    const m = minutes % 60;
+    return h > 0 ? `${h}h ${m}m` : `${m}m`;
+};
+
+const finalChartData = Object.entries(aggregatedData).filter(([, totalTimeSpent]) => totalTimeSpent > 60).map(([category, totalTimeSpent], index) => ({
+    id: index,
+    value: totalTimeSpent,
+    label: `${category} (${formatTime(totalTimeSpent)})`
+}))
+
+export default function TimeSpentChart() {
+
+
+    return (
+        <PieChart
+            series={[
+                {
+                    data: finalChartData,
+                    innerRadius: 45,
+                    valueFormatter: (item) => formatTime(item.value),
+                    arcLabelMinAngle: 25,
+                }
+            ]}
+            width={400}
+            height={300}
+        />
+    )
+
+}
