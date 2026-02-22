902f6b4963625e6cf04c8ae60a1eb5e3e7fdf49e
IngvaldssonMattias <mattias.ingvaldsson@chasacademyy.se>
2026-02-22 20:46:49 +0100

    put CalendarApp instide function

--- src/pages/StatisticsPage.jsx
+++ src/pages/StatisticsPage.jsx
@@ -2,6 +2,7 @@ import ProductivityChart from "../components/Statistics/ProductivityChart";
 import EnergyChart from "../components/Statistics/EnergyChart";
 import TimeSpentChart from "../components/Statistics/TimeSpentChart";
 import { mockData } from "../constants/mockData";
+import CalendarApp from "../components/Calendar/CalendarApp";
 
 function StatisticsPage() {
     return (
@@ -9,6 +10,7 @@ function StatisticsPage() {
         <ProductivityChart mockData={mockData} />
         <EnergyChart mockData={mockData} />
         <TimeSpentChart />
+        <CalendarApp />
     </>
     )
 }
