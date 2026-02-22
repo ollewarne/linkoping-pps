4ada16c9e5049de8493a57834ce6385b71f148d5
soew <209520777+ollewarne@users.noreply.github.com>
2026-02-22 17:28:28 +0100

    update to use new modal component

--- src/components/SettingsMenu/SettingsMenu.jsx
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -1,43 +1,16 @@
 import WorkDayForm from "../WorkDayForm/WorkDayForm";
-import { useState } from "react";
+import Modal from "../Modal/Modal";
 import styles from "./SettingsMenu.module.css";
 
 function SettingsMenu() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    function toggleMenu() {
-        setIsOpen(!isOpen);
-    }
-
-    function closeMenu() {
-        setIsOpen(false);
-    }
-
-    const handleBackdropClick = () => {
-        closeMenu();
-    };
-
-    const handlePanelClick = (e) => {
-        e.stopPropagation();
-    };
-
     return (
-        <>
-            <button onClick={toggleMenu} className={styles.settingsButton}>
+        <Modal trigger={
+            <button className={styles.settingsButton}>
                 <img src="/settings.svg" alt="Cogwheel settings button" />
             </button>
-
-            {isOpen && (
-                <div className={styles.overlay} onClick={handleBackdropClick}>
-                    <div className={styles.menuPanel} onClick={handlePanelClick}>
-                        <button onClick={closeMenu} className={styles.closeButton}>
-                            ✕
-                        </button>
-                        <WorkDayForm />
-                    </div>
-                </div>
-            )}
-        </>
+        }>
+            {({ onClose }) => <WorkDayForm onClose={onClose} />}
+        </Modal>
     );
 }
 
