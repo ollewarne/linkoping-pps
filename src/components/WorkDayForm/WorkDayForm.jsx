4e81c70bb1028e60d1e7bda119ac1c39c9e7d1dc
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-02-23 16:01:50 +0100

    fix localstorage bug

--- src/components/WorkDayForm/WorkDayForm.jsx
+++ src/components/WorkDayForm/WorkDayForm.jsx
@@ -39,8 +39,11 @@ export default function WorkDayForm() {
 
     // get saved workday values from localstorage to default input fields
     let workformData = getWorkdayFromStorage();
-    let nonWorkFormData = workformData.nonWorkHours;
-    console.log(workformData)
+    let nonWorkFormData = '';
+    if (workformData) {
+        nonWorkFormData = workformData.nonWorkHours;
+    }
+
 
 
     // ------------------ HANDLE SUBMIT ------------------
