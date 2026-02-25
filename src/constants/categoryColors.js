0192a10d79316db2435d1cf91af03aa175e5c808
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-02-25 10:51:18 +0100

    moved category color object since used in card as well

---
+++ src/constants/categoryColors.js
@@ -0,0 +1,14 @@
+export const categoryColors = {
+    'Meeting': 'var(--purple-meeting)',
+    'Administrative': 'var(--pink-admin)',
+    'Creative': 'var(--orange-creative)',
+    'Technical': 'var(--dark-blue-tech)',
+    'Analytical': 'var(--light-green-analytical)',
+    'Communication': 'var(--gold-communication)',
+    'Planning': 'var(--light-blue-planning)',
+    'Learning': 'var(--fuschsia-learning)',
+    'Sales & Marketing': 'var(--red-pink-sales)',
+    'Support': 'var(--dark-green-support)',
+    'Operations': 'var(--yellow-operations)',
+    'Other': 'var(--red-other)',
+}
