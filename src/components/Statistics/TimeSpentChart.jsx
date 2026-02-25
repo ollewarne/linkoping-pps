c267939db180a295e435d72a788a13da2f1c3383
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-02-25 10:12:40 +0100

    adding category colors to piechart

--- src/components/Statistics/TimeSpentChart.jsx
+++ src/components/Statistics/TimeSpentChart.jsx
@@ -1,6 +1,23 @@
 import { mockData } from "../../constants/mockData";
 import { PieChart } from "@mui/x-charts";
 
+
+const categoryColors = {
+    'Meeting': 'var(--purple-meeting)',
+    'Administrative': 'var(--pink-admin)',
+    'Creative': 'var(--orange-creative)',
+    'Technical': 'var(--dark-blue-tech)',
+    'Analytical': 'var(--light-green-analytical)',
+    'Communication': 'var(--gold-communication)',
+    'Planning': 'var(--light-blue-planning)',
+    'Learning': 'var(--fuschsia-learning)',
+    'Sales & Marketing': 'var(--red-pink-sales)',
+    'Support': 'var(--dark-green-support)',
+    'Operations': 'var(--yellow-operations)',
+    'Other': 'var(--red-other)',
+}
+
+
 const aggregatedData = Object.values(mockData).reduce((acc, day) => {
     day.activities.forEach(activity => {
         const { category, totalTimeSpent } = activity;
@@ -15,10 +32,13 @@ const formatTime = (minutes) => {
     return h > 0 ? `${h}h ${m}m` : `${m}m`;
 };
 
-const finalChartData = Object.entries(aggregatedData).filter(([, totalTimeSpent]) => totalTimeSpent > 60).map(([category, totalTimeSpent], index) => ({
-    id: index,
-    value: totalTimeSpent,
-    label: `${category} (${formatTime(totalTimeSpent)})`
+const finalChartData = Object.entries(aggregatedData)
+    .filter(([, totalTimeSpent]) => totalTimeSpent > 60)
+    .map(([category, totalTimeSpent], index) => ({
+        id: index,
+        value: totalTimeSpent,
+        label: `${category} (${formatTime(totalTimeSpent)})`,
+        color: categoryColors[category] || 'pink'
 }))
 
 export default function TimeSpentChart() {
