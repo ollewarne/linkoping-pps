da944534b697aa09114a61b8cc56c29ab5bc1648
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-03-02 15:22:06 +0100

    styling for cards

--- src/components/ActivityCard/ActivityCard.jsx
+++ src/components/ActivityCard/ActivityCard.jsx
@@ -39,8 +39,9 @@ function ActivityCard({ activity = {} }) {
     }
 
     return (
-        <div style={{ height: "20%", backgroundColor: backgroundColor }} className={styles.card}>
-            {editMode ? (
+        <div style={{borderColor: backgroundColor }} className={styles.card}>
+            {editMode ? 
+            (
                 <form className={styles.cardForm} onSubmit={(e) => {
                     submitEdit(e)
                     setEditMode(false)
@@ -56,25 +57,42 @@ function ActivityCard({ activity = {} }) {
                     </select>
                     <input name="timeStart" type="time" defaultValue={activity.scheduledTime.start} />
                     <input name="timeEnd" type="time" defaultValue={activity.scheduledTime.end} />
-                    <button type="submit">Save Activity</button>
-                    <button onClick={() => setEditMode(false)}>Cancel</button>
+
+                    <div className={styles['edit-confirm']}>
+                        <button type="submit" className={styles.editSubmit}>Save Activity</button>
+                        <button onClick={() => setEditMode(false)} className={styles.editCancel}>Cancel</button>
+                    </div>
                 </form>
             ) : (
                 <>
-                    <p className={styles.content}><strong>{activity.title}</strong> | <strong>{activity.category}</strong> | <strong>{activity.estimatedDuration} min</strong></p>
-                    <p><strong>Planned time</strong> | {activity.scheduledTime.start} - {activity.scheduledTime.end}</p>
+                <div className={styles.cardSpecificsTop}>
+                    <p className={styles.scheduled}>{activity.scheduledTime.start} - {activity.scheduledTime.end}</p>
+                    <p className={styles.category} style={{backgroundColor: backgroundColor}}>{activity.category}</p>
+                </div>
+
+                <p className={styles.title} style={{borderBottomColor: backgroundColor}}>{activity.title}</p>
+                            
+                <div className={styles.cardSpecificsBottom}>
+                    
+                    <p className={styles.duration}>Estimated duration: {activity.estimatedDuration} min</p>
+                    
                     {confirmDelete ? (
-                        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-                            <p>Are you sure?</p>
-                            <button className={styles.confirmYes} onClick={handleDelete}>Yes</button>
-                            <button className={styles.confirmNo} onClick={() => setConfirmDelete(false)}>No</button>
+                        <div className={styles.confirmContainer}>
+                            <p className={styles.confirmText}>Are you sure?</p>
+                            <button className={`${styles.confirmButton} ${styles.confirmYes}`} onClick={handleDelete}>Yes</button>
+                            <button className={`${styles.confirmButton} ${styles.confirmNo}`} onClick={() => setConfirmDelete(false)}>No</button>
                         </div>
                     ) : (
-                        <>
-                            <button disabled={activity.isActive} onClick={() => setEditMode(true)}>Edit Activity</button>
-                            <button disabled={activity.isActive} onClick={() => setConfirmDelete(true)}>Delete Activity</button>
-                        </>
+                        <div className={styles['utility-buttons']}>
+                            <button disabled={activity.isActive} onClick={() => setEditMode(true)}>
+                                <img src="/settings.svg" alt="Edit icon" />
+                            </button>
+                            <button disabled={activity.isActive} onClick={() => setConfirmDelete(true)}>
+                                <img src="/delete.svg" alt="Delete icon" />
+                            </button>
+                        </div>
                     )}
+                </div>
                 </>
             )}
         </div>
