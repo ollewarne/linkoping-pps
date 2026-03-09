4cb83f7834e46691f41566e5ae7de1d93ace5ea7
soew <209520777+ollewarne@users.noreply.github.com>
2026-03-09 12:34:29 +0100

    added a not found page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import DesktopLayout from "./layouts/DesktopLayout/DesktopLayout";
 import PlannerPage from "./pages/PlannerPage";
 import StatisticsPage from "./pages/StatisticsPage";
 import ActivityPage from "./pages/ActivityPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import { Route, Routes } from "react-router";
 
 import { useTranslator } from "./contexts/languageContext";
@@ -33,18 +34,20 @@ function App() {
         <PopupManager>
             {isMobile ? (
                 <Routes>
-                    <Route path="/" element={<MobileLayout />}>
+                    <Route path="/" end element={<MobileLayout />}>
                         <Route index element={<ActivityPage />} />
                         <Route path="planner" element={<PlannerPage />} />
                         <Route path="history" element={<StatisticsPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
                     </Route>
                 </Routes>
             ) : (
                 <Routes>
-                    <Route path="/" element={<DesktopLayout />}>
+                    <Route path="/" end element={<DesktopLayout />}>
                         <Route index element={<ActivityPage />} />
                         <Route path="planner" element={<PlannerPage />} />
                         <Route path="history" element={<StatisticsPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
                     </Route>
                 </Routes>
             )}
