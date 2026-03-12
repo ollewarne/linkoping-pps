bc6112eea1b44e5ef54c0f0bbf6ba8c63937334a
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-03-12 12:46:57 +0100

    history card component

---
+++ src/components/HistoryCard/HistoryCard.jsx
@@ -0,0 +1,34 @@
+import styles from "./HistoryCard.module.css";
+
+import { categoryColors } from "../../constants/categoryColors";
+
+// Kommer användas sen
+// import { useTranslator } from "../../contexts/languageContext";
+// import { languageLibrary } from "../../locales/language";
+
+
+function HistoryCard({activity = {}}) {
+    const categoryColor = categoryColors[activity.category];
+
+    return (
+        <div style={{borderColor: categoryColor}} className={styles.historyCard}>
+            <div className={styles.timeContainer}>
+                <p className={styles.time}>
+                    {activity.scheduledTimeStart} - {activity.scheduledTimeStop}
+                </p>
+                <img 
+                    src={ activity.isCompleted || activity.category === 'NonWork' ? "/activity-done.svg" : "/activity-missed.svg"} 
+                    alt="Done or missed check icon" 
+                    className={activity.isCompleted ? styles.done : styles.missed}
+                />
+            </div>
+
+            <p className={styles.title}>
+                {activity.title}
+            </p>
+            
+        </div>
+    )
+}
+
+export default HistoryCard;
