84a0922237b6534e843fd9666b6d2ec614e87060
IngvaldssonMattias <mattias.ingvaldsson@chasacademyy.se>
2026-03-17 14:59:34 +0100

    fixed css for smaller screen, tablets, phones

--- src/components/Calendar/DayDetails/DayDetails.jsx
+++ src/components/Calendar/DayDetails/DayDetails.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./DayDetailsPopup.css";
 
@@ -38,34 +39,6 @@ export const DayDetailsPopup = ({
                 <p>Category: {activity.category}</p>
                 <p>Scheduled time: {activity.scheduledTime}</p>
                 <p>total time: {activity.totalTimeSpent} min</p>
-               {/*
-                <p>Estimated time: {activity.estimatedDuration} minutes</p>
-                <p>Total time spent: {activity.totalTimeSpent} minutes</p>
-
-                {activity.isMeeting && (
-                  <p>
-                    Meeting time: {activity.meetingTimes.start} - {activity.meetingTimes.end}
-                  </p>
-                )}
-
-                <p>Active time: {activity.activeTime || "N/A"} minutes</p>
-                <p>Paused time: {activity.breakTime || "N/A"} minutes</p>
-                <p>
-                  Currently active: {activity.currentlyActive ? "Yes" : "No"}
-                </p>
-*/}
-                {/*
-                <div className="statistics">
-                  <h5>Statistics</h5>
-                  {Object.entries(activity.statistics).map(([time, stats]) => (
-                    <div key={time}>
-                      <p>Time: {time}</p>
-                      <p>Efficiency: {stats.efficiency}</p>
-                      <p>Productivity: {stats.productivity}</p>
-                      <p>Factor: {stats.factor || "No"}</p>
-                    </div> 
-                  ))}
-                </div> */}
               </div>
             ))}
           </div>
