a59cda79a6b68a922248e95a5801d69e653da6d0
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-03-19 15:09:33 +0100

    fix crash if no workday

--- src/utils/workdayStorage.js
+++ src/utils/workdayStorage.js
@@ -1,18 +1,27 @@
 const KEY = "workdayData";
 
 export function saveWorkdayToStorage(workdayData) {
-  localStorage.setItem(KEY, JSON.stringify(workdayData));
+    localStorage.setItem(KEY, JSON.stringify(workdayData));
 
-  window.dispatchEvent(new Event("workdayDataUpdated"));
+    window.dispatchEvent(new Event("workdayDataUpdated"));
 }
 
 export function getWorkdayFromStorage() {
-  const raw = localStorage.getItem(KEY);
-  if (!raw) return null;
+    const raw = localStorage.getItem(KEY);
+    if (!raw) {
+        const workday = {
+            workHours: { start: "08:00", end: "18:00" },
+            nonWorkHours: null,
+            workEnvironment: { location: "" }
+        }
+        saveWorkdayToStorage(workday)
+        return workday;
+    };
 
-  try {
-    return JSON.parse(raw);
-  } catch {
-    return null;
-  }
+    try {
+        return JSON.parse(raw);
+    } catch {
+        return null;
+    }
 }
+
